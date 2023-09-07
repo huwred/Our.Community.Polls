8bf2315b7be45a07668abba71573d22864c66229
huwred <huw@forum.snitz.com>
2023-09-07 14:05:00 +0100

    removed unnecesary file

--- Source/Our.Community.Polls/PollConstants/CacheRefresherConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Our.Community.Polls.PollConstants
-{
-    public class CacheRefresherConstants
-    {
-        public const string PollsCacheRefreshId = "D5A7DBAC-2E8E-4685-92FC-3F7602B2644C";
-
-    }
-}
